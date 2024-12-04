663aa3bac51708694834bceb2502449f7d5eb474
adedralin <adedralin@usfca.edu>
2024-12-04 11:26:02 -0800

    color

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ document.getElementById("colorForm").addEventListener("submit", function (event)
   // Backend URL
   const backendUrl = "https://colorserver.onrender.com/predict/";
 
-  document.getElementById("result").innerText = "Color Works";
+//   document.getElementById("result").innerText = "Color Works";
 
   // Fetch the prediction from the backend
   fetch(`${backendUrl}${hexColor}`)
